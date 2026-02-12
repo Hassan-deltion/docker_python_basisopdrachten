8880d4bd984b48fc29565042d6d9441fea7c38a7
Hassan-deltion <97097229@st.deltion.nl>
2026-02-12 13:26:00 +0100

    Opdracht 2 van 040

--- app/040_lists/opdr_2/opdr_2.py
+++ app/040_lists/opdr_2/opdr_2.py
@@ -1,15 +1,17 @@
 # Opdracht 2 lists
-# Naam student:
+# Naam student:Hassan Aljasem
 # Groep:
 
-
 rivier_info = {
-    "rijn": ["nederland", "duitsland", "Frankrijk"],
+    "rijn": ["nederland", "duitsland", "frankrijk"],
     "maas": ["nederland", "belgië", "duitsland"],
     "nijl": ["egypte", "soedan", "oeganda"]
 }
 
 rivieren = list(rivier_info.keys())
-# rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
-# Hier jouw code.....
+print(f"De rivier {rivieren[0].capitalize()} stroomt onder andere door {rivier_info[rivieren[0]][1].capitalize()}")
+
+print(f"De rivier {rivieren[1].capitalize()} stroomt onder andere door {rivier_info[rivieren[1]][0].capitalize()}")
+
+print(f"De rivier {rivieren[2].capitalize()} stroomt onder andere door {rivier_info[rivieren[2]][2].capitalize()}")
