8b18061708e8f988525f6ecc562fed15bfb8b8d2
Hassan-deltion <97097229@st.deltion.nl>
2026-02-12 13:41:39 +0100

    opdracht 3 van 050

--- app/050_input_function/opdr_3/opdr_3.py
+++ app/050_input_function/opdr_3/opdr_3.py
@@ -1,6 +1,14 @@
-# Opdracht 3 input functie
-# Naam student:
+# Naam student: Hassan Aljasem
 # Groep:
 
-# Hier komt je code...
+stad1 = input("Geef stad 1: ")
+stad2 = input("Geef stad 2: ")
+stad3 = input("Geef stad 3: ")
+stad4 = input("Geef stad 4: ")
+stad5 = input("Geef stad 5: ")
 
+steden = [stad1, stad2, stad3, stad4, stad5]
+
+steden.sort(reverse=True)
+
+print(steden)
