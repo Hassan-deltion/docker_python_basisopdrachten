675716501c4cbe6ea75a3c7b37407dbe8ad9aa26
Hassan-deltion <97097229@st.deltion.nl>
2026-03-12 11:32:12 +0100

    opdracht 3 van 070 12-03-2026

--- app/080_tekstbestanden/opdr_3/opdr_3.py
+++ app/080_tekstbestanden/opdr_3/opdr_3.py
@@ -2,5 +2,20 @@
 # Naam student:
 # Groep:
 
+tekst = input("Geef de tekst die wilt encrypten..\n")
 
+verschuiving = 5
+resultaat = ""
 
+for char in tekst:
+    if char.isalpha():
+        if char.islower():
+            nieuwe_code = (ord(char) - ord('a') + verschuiving) % 26 + ord('a')
+            resultaat += chr(nieuwe_code)
+        else:
+            nieuwe_code = (ord(char) - ord('A') + verschuiving) % 26 + ord('A')
+            resultaat += chr(nieuwe_code)
+    else:
+        resultaat += char
+
+print(resultaat)
