71403447d9c5cea4346d18e2b37218e4e6be5ea7
Hassan-deltion <97097229@st.deltion.nl>
2026-03-12 11:09:44 +0100

    oprdacht 5 12-03-2026

--- app/060_loops/opdr_5/opdr_5.py
+++ app/060_loops/opdr_5/opdr_5.py
@@ -1,9 +1,10 @@
 # Opdracht 3 input functie
-# Naam student:
+# Naam student:Hassan Aljasem
 # Groep:
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+my_list = [x * 3 for x in range(1, 20)]
+print(my_list[:6])
