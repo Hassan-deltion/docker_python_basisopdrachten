a7f7a9afc86ae3d74d32d473243a67368664026d
Hassan-deltion <97097229@st.deltion.nl>
2026-03-12 11:21:04 +0100

    opracht 3 van 070 12-03-2026

--- app/070_condities/opdr_3/opdr_3.py
+++ app/070_condities/opdr_3/opdr_3.py
@@ -10,3 +10,17 @@ kortings_percentages = {"baby": 100, "kinderen": 50, "volwassenen": 0, "ouderen"
 leeftijd = {"baby": (0, 2), "kinderen": (3, 18), "volwassenen": (19, 64), "ouderen": (65, 150)}
 
 # Hier komt je code...
+normale_toegangsprijs = 12.50
+kortings_percentages = {"baby": 100, "kinderen": 50, "volwassenen": 0, "ouderen": 30}
+leeftijd = {"baby": (0, 2), "kinderen": (3, 18), "volwassenen": (19, 64), "ouderen": (65, 150)}
+
+leeftijd_input = int(input("Geef uw leeftijd in aantal jaar\n"))
+
+for groep, (min_leeftijd, max_leeftijd) in leeftijd.items():
+    if min_leeftijd <= leeftijd_input <= max_leeftijd:
+        print(f"U behoort tot de groep {groep}")
+        korting = kortings_percentages[groep]
+        print(f"U krijgt {korting}% korting")
+        te_betalen = normale_toegangsprijs * (1 - korting / 100)
+        print(f"U betaalt daarom {te_betalen}")
+        break
