4d514984d78aa5eabc42ad78a4bdb2c848602a8b
Hassan-deltion <97097229@st.deltion.nl>
2026-03-12 11:25:36 +0100

    oprdacht 2 van 070 12-03-2026

--- app/080_tekstbestanden/opdr_1/opdr_1.py
+++ app/080_tekstbestanden/opdr_1/opdr_1.py
@@ -1,5 +1,17 @@
 # Opdracht 1 while-loops
-# Naam student:
+# Naam student: Hassan Aljasem
 # Groep:
 
 # Jouw code komt hier
+
+vraag1 = input("Wat vind je van de huidige regering?\n")
+vraag2 = input("Wat vind je van de Python-lessen tot nu toe?\n")
+vraag3 = input("Wat vind jij de mooiste stad van Nederland?\n")
+
+with open("enquete_resultaten.txt", "w") as bestand:
+    bestand.write("Antwoorden op de enquete:\n")
+    bestand.write(f"1. {vraag1}\n")
+    bestand.write(f"2. {vraag2}\n")
+    bestand.write(f"3. {vraag3}\n")
+
+print("Bedankt! Je antwoorden zijn opgeslagen.")
